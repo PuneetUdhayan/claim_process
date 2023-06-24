2bf642c703fb614199c8dcd7724ec7f7375f54b0
Puneet Udhayan <48212981+PuneetUdhayan@users.noreply.github.com>
2023-06-24 18:32:48 +0530

    set up containered service with postgressql

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,3 +1,5 @@
+import os
+
 from sqlalchemy.orm import Session
 
 
@@ -10,3 +12,18 @@ def db_session_handler(function: str, arguments: dict, db: Session):
     except Exception as e:
         db.rollback()
         raise e
+
+
+async def get_identifier(request) -> str:
+    """Rate limiter identifier has been overridden to use api key insead
+    of ip. Doing this ensures rate limit will be applied when multiple
+    API instances are running.
+
+    Args:
+        request (Request): provides request as input. This will be useful
+        if multiple users use the system.
+
+    Returns:
+        str: API key
+    """
+    return os.environ["API_KEY"]
